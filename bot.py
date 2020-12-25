34f182b5dbe55fbd1565de410c972c9f4a1fab73
Skid <mikopr@wp.pl>
2020-12-25 19:10:14 +0100

    make it better

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 import os
 import discord
 
-
-TOKEN = 'NzE5MjY1MzgyMTI0MjI0NjA0.Xt0_zg.26lDdEyu_WXpTOGDbKrN8Gr7OjE'
+TOKEN = "NzkxMzE2MjAyOTc0MjE2MjQy.X-NYpA.hYCliU9r1uKyFI7jlYOEzHPyN-o"
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game(name="Napisz, aby coś zgłosić!"))
+    await client.change_presence(activity=discord.Game(name="Pilnowanie Zagubionych"))
 
 
 @client.event
@@ -55,48 +55,59 @@ async def on_raw_reaction_add(payload):
 
 @client.event
 async def on_message(message):
-    if not message.guild:
-        if not message.author.bot:
-            await message.channel.send("""Dzięki za zgłoszenie!
+    if not message.guild and message.content.startswith("/zgłaszam"):
+       	await message.channel.send("""Dzięki za zgłoszenie!
 Zostało ono przekazane do teamu i będzie rozpatrzone w niedalekiej przyszłości.
 """)
-            kanal = client.get_channel(759148536930762783)
-            member_ping = '<@' + str(message.author.id) + '>'
-            await kanal.send(member_ping + """ 
-""" + message.content + '\n-------------------------------')
+       	kanal = client.get_channel(759148536930762783)
+       	member_ping = '<@' + str(message.author.id) + '>'
+       	await kanal.send(member_ping + """ 
+""" + message.content[10:] + '\n-------------------------------')
 
     elif "Przyzywam" in message.content and message.author.id == 349606518594732055:
         guild = client.get_guild(567043766108815381)
-        member_id = message.content[10:28]
-        ilosc = message.content[29:]
+        skid = await guild.fetch_member(349606518594732055)
+        skidchannel = skid.voice.channel
+        kanal = await skidchannel.create_invite(max_age = 300, max_users = 1)
+        member_id = message.mentions[0].id
         member = await guild.fetch_member(member_id)
         x = 0
         await message.add_reaction("PeepoYes:647938639283879944")
-        while x < int(ilosc):
-            await member.send("KTOŚ CIĘ WZYYYYYYYWA")
+        while x < 4:
+            await member.send("Skid Cię potrzebuje! Odpowiesz na wezwanie?")
             x +=1
-        await message.channel.send(str(member) + " został przyzwany " + str(x) + " razy <:Pepega:590993310055792640>")
+        await member.send(kanal)
+        await message.channel.send(str(member) + " został przyzwany <:Pepega:590993310055792640>")
     
-    elif "AWATAR" in message.content.upper():
-        if len(message.content) == 6:
+    elif "/AWATAR" in message.content.upper():
+        if len(message.content) == 7:
             member = message.author
         else:
             guild = client.get_guild(567043766108815381)
-            member_id = message.content[7:]
+            member_id = message.mentions[0].id
             member = await guild.fetch_member(member_id)
         
         await message.channel.send(str(member.avatar_url))
         await message.delete()
 
-    elif message.content.upper() == "DISCORDOWE ID":
+    elif message.content.upper() == "/DISCORDOWE ID":
         await message.author.send("Twoje ID to " + str(message.author.id))
         await message.delete()
 
-    elif message.attachments and message.channel.id == 711606924797280348:
-        await message.add_reaction("PeepoYes:647938639283879944")
-        await message.add_reaction("peepoNo:647938989742882816")
+    elif message.attachments:
+        if message.channel.id == 789188671584600126 or message.channel.id == 711606924797280348:
+        	await message.add_reaction("PeepoYes:647938639283879944")
+        	await message.add_reaction("peepoNo:647938989742882816")
+
+    elif message.content == "/help":
+            await message.author.send("""Witaj dzielny Strażniku! Oto lista moich funkcji!
         
-    
+/awatar [@ktoś] - wyświetla awatar osoby pingowanej, wyświetli Twój jeżeli żaden nie został podany
+/discordowe id - wysyła wiadomość prywatną z Twoim discordowym ID
+
+W razie problemów nie bój się napisać do Skid#7847""")
+            await message.delete()
+
 
 
 
