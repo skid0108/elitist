13dd2b0dcc426396d0b7b279a9b8d84b656d07e9
= <mikopr@wp.pl>
2022-03-09 23:45:26 +0100

    make it better

--- bot.py
+++ bot.py
@@ -40,6 +40,30 @@ opcje = {
     "NAJLEPSZEGO": 1
 }
 
+ratio = {
+    1: "https://media.giphy.com/media/JONVSqlDvbKfrkK1ys/giphy.gif",
+    2: "https://tenor.com/view/trump-donaldtrump-interview-banned-cnn-gif-7677105",
+    3: "https://imgur.com/jIxteBQ",
+    4: "https://gfycat.com/simpledeliriousasianporcupine",
+    5: "https://media.discordapp.net/attachments/798363780714856500/921838687795429376/jameswerk.gif",
+    6: "https://media.discordapp.net/attachments/813334484039630910/941355773764730921/ezgif.com-gif-maker_23.gif",
+    7: "https://gfycat.com/biodegradablegrosskoi",
+    8: "https://media.giphy.com/media/pvXPiREYPUSTo4fbBv/giphy-downsized-large.gif",
+    9: "https://media4.giphy.com/media/fUp4znpeoWm4HgPwre/giphy.mp4?cid=73b8f7b170a931f372b250157974124fc308656ba1eb7e83&rid=giphy.mp4&ct=g",
+    10: "https://media.giphy.com/media/oop0T7d8GhfgbxMYd9/giphy.gif",
+    11: "https://media.discordapp.net/attachments/827552926360272936/910167807126306916/ratio1.gif",
+    12: "https://tenor.com/view/trump-donaldtrump-interview-banned-cnn-gif-7677105",
+    13: "https://media.discordapp.net/attachments/922000064262574110/926049849739190272/8C43B7B9-DA30-4E91-9F28-43C3092D78BE-1280-000000C28D43E321.gif",
+    14: "https://media.giphy.com/media/P4R5fySS7gl0nS5t7H/giphy.gif",
+    15: "https://media.discordapp.net/attachments/827552926360272936/910167807126306916/ratio1.gif",
+    16: "https://media.discordapp.net/attachments/484153257753837568/913226774329065552/vog_ratio.gif",
+    17: "https://gfycat.com/caringgloomyhalicore",
+    18: "https://media.giphy.com/media/9YX4P3CFrXsTNBCPAQ/giphy.gif",
+    19: "https://media.discordapp.net/attachments/809286191538896966/899145578561302548/ratiorakka.gif",
+    20: "https://imgur.com/a/itiUO4Q",
+    21: "https://gfycat.com/wanaggressiveivorybackedwoodswallow",
+}
+
 
 
 @client.event
@@ -92,6 +116,11 @@ Pojawił się nowy rekrut, """ + user.mention + "!")
 
 @client.event
 async def on_message(message):
+    if "RATIO" in message.content.upper():
+        await message.channel.send(ratio[randint(1, 21)])
+
+
+
     if message.content == "rgbjk":
         await message.channel.send("ok")
         guild = client.get_guild(567043766108815381)
@@ -174,6 +203,18 @@ W razie problemów nie bój się napisać do Skid#7847""")
                     await message.channel.send("Zyebaueś")
             except asyncio.TimeoutError:
                 await message.channel.send("Zyebaueś")
+
+    elif "WESPINGUJ" in message.content.upper() and message.author.id == 349606518594732055:
+        role = message.role_mentions[0]
+        guild = message.guild
+        msg = ''
+        xd = 1
+        for x in  guild.members:
+            if role in x.roles:
+                msg += (f"{xd}. <@{x.id}>\n")
+                xd+=1
+        await message.channel.send(msg)
+
 
 @client.event
 async def on_member_join(member):
